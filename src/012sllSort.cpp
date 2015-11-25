3c57731c1254dbbce62166c17e1e37d4da5c9b19
dasamantaraoaditya <dasamantarao.aditya@gmail.com>
2015-11-25 11:44:50 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -1,5 +1,5 @@
 /*
-OVERVIEW:  Given a single linked list of 0s 1s and 2s ,Sort the SLL such that zeroes 
+OVERVIEW:  Given a single linked list of 0s 1s and 2s ,Sort the SLL such that zeroes
 will be followed by ones and then twos.
 
 
@@ -20,7 +20,35 @@ struct node {
 	int data;
 	struct node *next;
 };
-
 void sll_012_sort(struct node *head){
-	
+	int count0 = 0, count1 = 0, count2 = 0;
+	struct node *inter;
+	inter = head;
+	while (head != '\0')
+	{
+		if (head->data == 1)
+			count1++;
+		else if (head->data == 0)
+			count0++;
+		else
+			count2++;
+		head = head->next;
+	}
+
+
+	while (count0--)
+	{
+		inter->data = 0;
+		inter = inter->next;
+	}
+	while (count1--)
+	{
+		inter->data = 1;
+		inter = inter->next;
+	}
+	while (count2--)
+	{
+		inter->data = 2;
+		inter = inter->next;
+	}
 }
--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -6,7 +6,7 @@ INPUTS:  A number.
 
 OUTPUT: Create linked list from given number, each digit as a node.
 
-ERROR CASES: 
+ERROR CASES:
 
 NOTES: For negative numbers ignore negative sign.
 */
@@ -18,7 +18,46 @@ struct node {
 	int num;
 	struct node *next;
 };
-
+struct node * reverse_linkedlist(struct node *start);
+void insert_at_end(struct node *end, int n);
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	struct node *head = NULL, *end = NULL, *start = NULL;
+	if (N<0)
+		N *= -1;
+	end = (struct node *)malloc(sizeof(struct node));
+	head = end;
+	start = end;
+	end->num = N % 10;
+	end->next = NULL;
+	N /= 10;
+	while (N)
+	{
+		insert_at_end(end, N % 10);
+		N /= 10;
+	}
+	head = reverse_linkedlist(start);
+	return head;
+}
+
+void insert_at_end(struct node *end, int n)
+{
+	struct node *temp;
+	temp = (struct node *)malloc(sizeof(struct node));
+	temp->num = n;
+	temp->next = NULL;
+	while (end->next != NULL)
+		end = end->next;
+	end->next = temp;
+}
+
+struct node * reverse_linkedlist(struct node *start)
+{
+	struct node *previous = NULL;
+	while (start) {
+		struct node *temp = start;
+		start = start->next;
+		temp->next = previous;
+		previous = temp;
+	}
+	return previous;
 }
